a4866db60301870f9bb9399772947a1e472f4538
winescout <matt@notch8.com>
2019-01-11 02:42:39 -0800

    wip: adding choice page

--- android/app/src/main/java/com/learnacademy/mobile/MainApplication.java
+++ android/app/src/main/java/com/learnacademy/mobile/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import io.invertase.firebase.RNFirebasePackage;
-import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -41,7 +40,8 @@ public class MainApplication extends NavigationApplication {
       // Add additional packages you require here
       // No need to add RnnPackage and MainReactPackage
       return Arrays.<ReactPackage>asList(
-              new VectorIconsPackage()
+              new VectorIconsPackage(),
+              new RNFirebasePackage()
       );
   }
 
--- src/LearnScreens/LoginScreen/LoginScreen.js
+++ src/LearnScreens/LoginScreen/LoginScreen.js
@@ -8,6 +8,10 @@ import {
   Text,
   TouchableOpacity
 } from 'react-native';
+import {
+  Button,
+  Icon,
+} from 'react-native-elements'
 import { connectData } from 'AppRedux';
 import { apiConfig } from 'AppConfig';
 import { pushAuthenticated } from 'LearnNavigation';
@@ -29,12 +33,73 @@ class LoginScreen extends PureComponent {
     fetchData({ url: apiConfig.api_endpoint, method: 'GET', params: null });
   }
 
+  onLoginOrRegister = () => {
+  const { phoneNumber } = this.state;
+  firebase.auth().signInWithPhoneNumber(phoneNumber)
+    .then((confirmResult) => {
+      // This means that the SMS has been sent to the user
+      // You need to:
+      //   1) Save the `confirmResult` object to use later
+      this.setState({ confirmResult });
+      //   2) Hide the phone number form
+      //   3) Show the verification code form
+    })
+    .catch((error) => {
+      const { code, message } = error;
+      // For details of error codes, see the docs
+      // The message contains the default Firebase string
+      // representation of the error
+    });
+  }
+  onVerificationCode = () => {
+    const { confirmResult, verificationCode } = this.state;
+    confirmResult.confirm(verificationCode)
+      .then((user) => {
+        // If you need to do anything with the user, do it here
+        // The user will be logged in automatically by the
+        // `onAuthStateChanged` listener we set up in App.js earlier
+      })
+      .catch((error) => {
+        const { code, message } = error;
+        // For details of error codes, see the docs
+        // The message contains the default Firebase string
+        // representation of the error
+      });
+  }
+
   render() {
     return (
       <View style={styles.flex}>
-        <TouchableOpacity onPress={() => pushAuthenticated()}>
-          <Text>Click here!</Text>
-        </TouchableOpacity>
+        <Icon
+            name="comment"
+            type='font-awesome'
+            size={50}
+            color='red'
+          />
+        <Icon
+            name='github-square'
+            type='font-awesome'
+            size={50}
+            color='red'
+          />
+        <Button
+          icon={{
+            name: "comment",
+            type: 'font-awesome',
+            size: 50,
+            color: 'white',
+          }}
+          title="SMS Login"
+        />
+        <Button
+          icon={{
+            name: 'github-square',
+            type: 'font-awesome',
+            size: 50,
+            color: 'white',
+          }}
+          title="Github Login"
+        />
       </View>
     );
   }
